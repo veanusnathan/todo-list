1afefd7344a35e7679d061e74939f0800edeb90d
Veanus Nathan <veanusnathan.work@gmail.com>
2023-12-15 16:23:21 +0700

    resolve

--- src/features/home/todoCard.tsx
+++ src/features/home/todoCard.tsx
@@ -23,10 +23,10 @@ const ToDoCard = ({
       } p-4 w-full rounded-[16px] flex flex-col justify-start`}
       onClick={() => push(`/todo-list?id=${id}`)}
     >
-      <p className="1024:text-2xl 375:text-lg 768:text-xl line-clamp-1">
+      <p className="1024:text-2xl 375:text-lg 768:text-xl line-clamp-1 w-full text-start">
         {title || "New ToDos"}
       </p>
-      <p className="mt-[14px] py-[3px] px-[7px] rounded-full text-white bg-black 375:text-xs">
+      <p className="mt-[14px] py-[3px] px-[7px] rounded-full text-white bg-black 375:text-xs 280:text-[10px] 430:text-sm">
         {label || "No Label"}
       </p>
     </button>
